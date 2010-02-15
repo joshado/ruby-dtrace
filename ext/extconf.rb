6ad06d03ffd56fae6594ca82f66b25f350261157
Chris Andrews <chris@nodnol.org>
2010-02-15 17:40:16 +0000

    Improve platform detection in setup.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,6 @@
 require 'mkmf'
 require 'rbconfig'
+require 'getoptlong'
 
 def symlink(old, new)
   begin
@@ -10,22 +11,53 @@ def symlink(old, new)
   end
 end
 
+opts = GetoptLong.new([ '--arch', '-a', GetoptLong::REQUIRED_ARGUMENT ])
+
+arch = nil
+opts.each do |opt, arg|
+  case opt
+  when '--arch'
+    arch = arg
+  end
+end
+
 $CFLAGS += " -D_LONGLONG_TYPE -g"
 have_library("dtrace", "dtrace_open")
 
-# Update machine-dependent symlinks in the source, based on $Config::CONFIG and `uname -p`
-os  = Config::CONFIG['target_os']
-os.gsub!  /[0-9.]+$/, ''
+unless arch
 
-# On OSX, this is "powerpc", even on Intel...
-if os == 'darwin'
-  cpu = Config::CONFIG['ARCH_FLAG']
-  cpu.gsub!(/-arch /, '')
+  # Attempt to figure out the build arch automatically...
+  # We want an cpu-os combination.
+  
+  # TODO - catch pre 10.5 OSX and pre 10 Solaris.
+  os  = Config::CONFIG['target_os']
+  os.gsub!  /[0-9.]+$/, ''
+
+  if os == 'darwin'
+    if Config::CONFIG['ARCH_FLAG'] != ''
+      # Single-arch build (macports, 10.5)
+      cpu = Config::CONFIG['ARCH_FLAG']
+      cpu.gsub!(/-arch /, '')
+    else
+      # Multi-arch build (Apple, 10.6)
+      # TODO - real multi-arch
+      cpu = 'x86_64'
+    end
+  else
+    # Solaris 64 bit Ruby builds seem to be done by: CC="gcc -m64"
+    # TODO - sunstudio builds?
+    cpu = Config::CONFIG['CC'] =~ /64/ ? 'x86_64' : 'i386'
+  end
+
+  dir = "#{cpu}-#{os}"
+
+  puts "Selected architecture: #{dir}"
+  puts "(override with --arch=...)"
 else
-  cpu = Config::CONFIG['CC'] =~ /64/ ? 'x86_64' : 'i386'
+  dir = arch
+  puts "Using overridden architecture: #{dir}"
 end
 
-dir = "#{cpu}-#{os}"
 symlink "#{dir}/dtrace_probe.c", "dtrace_probe.c"
 
 # Create makefile in the usual way
